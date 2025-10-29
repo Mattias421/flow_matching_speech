084bc27bcfaa745e346cafc2110c03c77c93c927
acq22mc <acq22mc@login1.pri.stanage.alces.network>
2025-10-29 13:44:24 +0000

    update config for stanage, prepare for kenlm

---
+++ examples/text/xps/kenlm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=80:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --output=logs/slurm/%x-%a-2.out
+#SBATCH --export=NONE
+
+echo "starting experiment"
+
+cd $EXP/flow_matching_speech/examples/text
+ml GCC GCCcore binutils libsndfile cuDNN
+source .venv/bin/activate
+export SUBMITIT_EXECUTOR=slurm
+python run_train.py --config-name librispeech data.cache_dir=$HF_DATASETS_CACHE hydra_dir=./outputs eval.kenlm_path=$KENLM -m
+
+echo "multirun complete"
+
+
