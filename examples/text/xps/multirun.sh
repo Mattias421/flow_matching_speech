876d3be06dc0075c1533225fd10a963007b56cc7
acq22mc <acq22mc@login1.pri.stanage.alces.network>
2026-01-30 18:57:02 +0000

    stop using submitit, using slurm arrays instead

--- examples/text/xps/multirun.sh
+++ examples/text/xps/multirun.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#SBATCH --time=80:00:00
+#SBATCH --array=0-960
+#SBATCH --time=5:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=96G
-#SBATCH --output=logs/slurm/%x-%a-2.out
+#SBATCH --mem=96G
 #SBATCH --export=NONE
+#SBATCH --output=logs/slurm/%x-%a-2.out
+#SBATCH --partition=gpu,gpu-h100,gpu-h100-nvl
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
 
 echo "starting experiment"
 
@@ -11,9 +15,38 @@ cd $EXP/flow_matching_speech/examples/text
 ml GCC GCCcore binutils libsndfile
 source .venv/bin/activate
 export SUBMITIT_EXECUTOR=slurm
-python run_train.py --config-name librispeech data.cache_dir=$HF_DATASETS_CACHE data.features_path=$DATA/LibriSpeech-Clean-NoSil/features/mimi/ hydra_dir=./outputs  study_name=the_big_rando -m
 
+counter=0
 
-echo "multirun complete"
+for lr in 0.0001 0.0002 0.0003 0.0004; do
+for warmup in 500 1000 1500 2000; do
+    for t_min in 0.0 0.5 0.75 0.9 1.0; do
+        for n_quantizers in 32 64 100 200; do
+            for quantizer_groups in 1 2 3; do
+    if [ "$SLURM_ARRAY_TASK_ID" == "$counter" ]; then
+
+        echo "running job $counter with $lr lr"
+
+        python run_train.py -m --config-name librispeech \
+        hydra.job.num=$SLURM_ARRAY_TASK_ID \
+        data.text_data=$DATA/variety-text-corpus/LibriLM/text/phones \
+        data.features_path=$DATA/LibriSpeech-10hr-rVAD/features/wav2vec_vox \
+        hydra_dir=./outputs  \
+        study_name=the_big_rando_2 \
+        optim.lr=$lr \
+        optim.warmup=$warmup \
+        training.t_min=$t_min \
+        model.n_quantizers=$n_quantizers \
+        model.quantizer_groups=$quantizer_groups
+fi
+    counter=$(( counter + 1 ))
+
+done
+done
+done
+done
+done
+
+echo "jobs done"
 
 
